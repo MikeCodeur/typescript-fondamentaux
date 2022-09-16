d6d6f7b483f17ca847b5688ef64a016ff5924580
Mike Codeur <cabanas.mickael@gmail.com>
2022-09-16 17:50:35 +0800

    exo 9

---
+++ src/exercise/09.ts
@@ -0,0 +1,11 @@
+// Les fonctions
+// http://localhost:3000/alone/exercise/08.ts
+
+// ❌ NE PAS MODIFIER
+// Utilitaire ne faisant pas partie de l'exercice
+import displayText, {init} from './helper/exerciseHelper'
+init()
+
+/*eslint
+  @typescript-eslint/no-unused-vars: "off"
+*/
